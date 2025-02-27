c4fe48c4d4ebaeefe2454e279a6986aeb0a3af2d
virtualmachine44 <bojda2@uniba.sk>
2025-02-27 23:11:23 +0100

    Language.tsx with bootstrap.

--- src/components/Language.tsx
+++ src/components/Language.tsx
@@ -1,21 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { RootState, AppDispatch } from '../store/store';
+import { AppDispatch } from '../store/store';
 import { selectConstants, selectPredicates, selectParsedConstants, selectParsedPredicates, updateCValue, updatePValue } from '../store/languageSlice';
 import 'katex/dist/katex.min.css';
-import { InlineMath, BlockMath } from 'react-katex';
+import { InlineMath } from 'react-katex';
 import SyntErr from './SyntErr';
 import { selectLanguage } from '../store/languageSlice';
-import { Form, Container, Row, Col } from 'react-bootstrap';
+import { Form, Row, Col, InputGroup, Card } from 'react-bootstrap';
 
 const Language: React.FC = () => {
   const constants = useSelector(selectConstants);
   const predicates = useSelector(selectPredicates);
   const parsedConstants = useSelector(selectParsedConstants);
   const parsedPredicates = useSelector(selectParsedPredicates);
-  //const [error, setError] = useState<SyntaxError | null>(null);
   const dispatch = useDispatch<AppDispatch>();
-  const { language, factories, error } = useSelector(selectLanguage); 
+  const { error } = useSelector(selectLanguage);
 
   if (parsedConstants.error) {
     console.log(parsedConstants.error);
@@ -26,47 +25,69 @@ const Language: React.FC = () => {
   }
 
   if (error) {
-    console.log(error)
+    console.log(error);
   }
 
   return (
-    <Container>
-      <h1>Language</h1>
-      <Row className="input-container">
-        <Col>
-          <Form.Group controlId="constantsInput">
-            <Form.Label>
-              <InlineMath math="\mathcal{C}_\mathcal{L} =" />
-            </Form.Label>
-            <Form.Control
-              type="text"
-              maxLength={100}
-              value={constants}
-              onChange={(e) => dispatch(updateCValue(e.target.value))}
-              className="input-field"
-            />
-            <SyntErr inputString={constants} error={parsedConstants.error} />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row className="input-container">
-        <Col>
-          <Form.Group controlId="predicatesInput">
-            <Form.Label>
-              <InlineMath math="\mathcal{P}_\mathcal{L} =" />
-            </Form.Label>
-            <Form.Control
-              type="text"
-              maxLength={100}
-              value={predicates}
-              onChange={(e) => dispatch(updatePValue(e.target.value))}
-              className="input-field"
-            />
-            <SyntErr inputString={predicates} error={parsedPredicates.error} />
-          </Form.Group>
-        </Col>
-      </Row>
-    </Container>
+    <Card>
+      <Card.Header>Language</Card.Header>
+      <Card.Body>
+        <Row>
+          <Col>
+            <Form.Group controlId="constantsInput" className='mb-3'>
+              <Form.Label>
+                Individual constants
+              </Form.Label>
+              <InputGroup>
+                <InputGroup.Text>
+                  <InlineMath math="\mathcal{C}_\mathcal{L} = \{" />
+                </InputGroup.Text>
+                  <Form.Control
+                    type="text"
+                    value={constants}
+                    onChange={(e) => dispatch(updateCValue(e.target.value))}
+                    className="input-field"
+                    isInvalid={!!parsedConstants.error}
+                  />
+                <InputGroup.Text>
+                  <InlineMath math="\}" />
+                </InputGroup.Text>
+                <Form.Control.Feedback type="invalid">
+                  <SyntErr inputString={constants} error={parsedConstants.error} />
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group controlId="predicatesInput" className='mb-3'>
+              <Form.Label>
+                Predicate symbols
+              </Form.Label>
+              <InputGroup>
+                <InputGroup.Text>
+                  <InlineMath math="\mathcal{P}_\mathcal{L} = \{" />
+                </InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  value={predicates}
+                  onChange={(e) => dispatch(updatePValue(e.target.value))}
+                  className="input-field"
+                  isInvalid={!!parsedPredicates.error}
+                />
+                <InputGroup.Text>
+                  <InlineMath math="\}" />
+                </InputGroup.Text>
+                <Form.Control.Feedback type="invalid">
+                  <SyntErr inputString={predicates} error={parsedPredicates.error} />
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
   );
 };
 
